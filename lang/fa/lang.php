6638a280b8230f457f3e6ac34d0ebe908a6f213e
Ramin Firooz <eleram.com@gmail.com>
2018-01-18 15:31:08 +0330

    Add Farsi/Persian language.

---
+++ lang/fa/lang.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => ' ویرایش‌گر های Wysiwyg',
+        'description' => 'وارد کردن ویرایش‌گر Wysiwyg مورد علاقه به سیستم مدیریت محتوا و افزونه ها...'
+    ],
+    'settings' => [
+        'label' => ' ویرایش‌گر های Wysiwyg',
+        'description' => 'ویرایش تنظیمات ویرایش‌گر های Wysiwyg'
+    ],
+    'widget' => [
+        'label' => 'Wysiwyg',
+        'name' => ' ویرایش‌گر های Wysiwyg',
+        'description' => 'Renders a wysiwyg editor of user`s choice'
+    ],
+    'form' => [
+        'settings' => [
+            'select_editor' => 'انتخاب ویرایش‌گر',
+            'editor_width' => 'عرض ویرایش‌گر',
+            'editor_height' => 'ارتفاع ویرایش‌گر',
+            'toolbar_label' => 'سفارش سازی نوار ابزار',
+            'toolbar_tinymce' => 'مستندات: http://www.tinymce.com/docs/advanced/editor-control-identifiers/#toolbarcontrols',
+            'toolbar_ckeditor' => 'مستندات: http://docs.ckeditor.com/#!/guide/dev_toolbar'
+        ],
+        'page' => [
+            'label' => 'استفاده در سیستم مدیریت محتوا - صفحات',
+            'comment' => ''
+        ],
+        'content' => [
+            'label' => 'استفاده در سیستم مدیریت محتوا - محتوا',
+            'comment' => ''
+        ],
+        'partial' => [
+            'label' => 'استفاده در سیستم مدیریت محتوا - بخش ها',
+            'comment' => ''
+        ],
+        'layout' => [
+            'label' => 'استفاده در سیستم مدیریت محتوا - طرح بندی ها',
+            'comment' => ''
+        ],
+        'others' => [
+            'label' => 'استفاده در مابقی؟',
+            'comment' => 'جایگزین کردن همه ویرایش‌گر ها با ویرایش‌گر Wysiwyg در همه افزونه ها به جز (صفحات، بخش ها، طرح بندی ها و محتوا)'
+        ],
+        'spages' => [
+            'label' => 'استفاده در صفحات ایستای RainLab ؟',
+            'comment' => 'استفاده از ویرایشگر Wysiwyg در افزونه RainLab Static Pages به عنوان ویرایشگر صفحه'
+        ],
+        'blog' => [
+            'label' => 'استفاده در RainLab Blog ؟',
+            'comment' => 'استفاده از ویرایشگر Wysiwyg در افزونه RainLab Blog به عنوان ویرایشگر مطلب'
+        ],
+        'problog' => [
+            'label' => 'استفاده در Radiantweb Problog ؟',
+            'comment' => 'استفاده از ویرایشگر Wysiwyg در افزونه Radiantweb Problog به عنوان ویرایشگر مطلب'
+        ],
+        'proevent' => [
+            'label' => 'استفاده در Radiantweb ProEvents ؟',
+            'comment' => 'استفاده از ویرایشگر Wysiwyg در افزونه Autumn Pages به عنوان ویرایشگر جزئیات رخدادها'
+        ],
+        'apages' => [
+            'label' => 'استفاده در صفحات Autumn ؟',
+            'comment' => 'استفاده از ویرایشگر Wysiwyg در افزونه Autumn Pages به عنوان ویرایشگر صفحه'
+        ],
+        'cplus' => [
+            'label' => 'استفاده در Indikator Content Plus ؟',
+            'comment' => 'استفاده از ویرایشگر Wysiwyg در افزونه Indikator Content Plus به عنوان ویرایشگر محتوا'
+        ],
+        'news' => [
+            'label' => 'استفاده در Indikator News & Newsletter ؟',
+            'comment' => 'استفاده از ویرایشگر Wysiwyg در افزونه Indikator News & Newsletter به عنوان ویرایشگر اخبار'
+        ],
+        'tab' => [
+            'settings' => 'تنظیمات',
+            'content' => 'محتوی',
+            'section' => 'افزونه ها'
+        ]
+    ]
+];
